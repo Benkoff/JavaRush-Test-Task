7a5f383e3b57799bfe2a9b651ec40d7c006b948a
Benkoff <bigopendatanet@yahoo.com>
2017-10-18 03:09:01 +0300

    maven project started

---
+++ spring5books/src/main/java/io/github/benkoff/spring5books/Spring5booksApplication.java
@@ -0,0 +1,12 @@
+package io.github.benkoff.spring5books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5booksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5booksApplication.class, args);
+	}
+}
---
+++ spring5books/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS book;
+CREATE TABLE IF NOT EXISTS book(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    title VARCHAR(100),
+    description VARCHAR(255),
+    author VARCHAR(100),
+    isbn VARCHAR(20),
+    printYear INT,
+    readAlready BIT NOT NULL,
+    PRIMARY KEY(id)
+) ENGINE = InnoDB;
+INSERT INTO book (title, description, author, isbn, printYear, readAlready) VALUES
+    ('First Book', 'just a book', 'A.Author', '001001', 2010, 0),
+    ('Second Book', 'just another book', 'B.Author', '001002', 2010, 1),
+    ('Third Book', 'yet another book', 'C.Author', '001003', 2010, 0),
+    ('Forth Book', 'this book has number 4', 'D.Author', '53t626', 2000, 1),
+    ('Fith Book', 'the book marked #5', 'E.E.Author', '256434', 2019, 0);
