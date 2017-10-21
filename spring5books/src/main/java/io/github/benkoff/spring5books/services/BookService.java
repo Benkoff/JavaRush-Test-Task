004c31706cf5196901c911b0cedd670f86fe677c
Benkoff <bigopendatanet@yahoo.com>
2017-10-21 22:34:51 +0300

    minor improvements

--- spring5books/src/main/java/io/github/benkoff/spring5books/services/BookService.java
+++ spring5books/src/main/java/io/github/benkoff/spring5books/services/BookService.java
@@ -1,10 +1,8 @@
 package io.github.benkoff.spring5books.services;
 
 import io.github.benkoff.spring5books.model.Book;
-import org.springframework.stereotype.Service;
 import java.util.Set;
 
-@Service
 public interface BookService {
     Set<Book> getAllBooks();
 }
--- spring5books/src/main/java/io/github/benkoff/spring5books/services/BookServiceImpl.java
+++ spring5books/src/main/java/io/github/benkoff/spring5books/services/BookServiceImpl.java
@@ -2,10 +2,12 @@ package io.github.benkoff.spring5books.services;
 
 import io.github.benkoff.spring5books.model.Book;
 import io.github.benkoff.spring5books.repositories.BookRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
 
